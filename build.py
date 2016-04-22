0d332dbaa01b98142ff1ab67fa16ca53c63ba293
wolfgang <wolfgang@decatur.de>
2016-04-22 21:34:49 +0200

    doc templating is sep driven

--- build.py
+++ build.py
@@ -1,6 +1,8 @@
 import codecs
 import xml.etree.ElementTree as ET
 import logging
+import textwrap
+import re
 
 logging.basicConfig(level=20) # info
 
@@ -14,10 +16,9 @@ f.close()
 def mergeText( readme, sep, text ):
     c = readme.split('[//]: # "%s"' % sep)
     if len(c) != 3:
-        #logging.warning("Skip %s" % sep)
+        logging.error("Invalid separator %s" % sep)
         return readme
 
-    logging.info("Replacing %s" % sep)
     c[1] = text + '\n'
     readme = ('[//]: # "%s"' % sep).join(c)
     return readme
@@ -27,39 +28,50 @@ def mergeElement( readme, sep, element ):
     for child in element:
         if child.text == None:
             continue
-        elif child.tag == 'matlab':
-            text = text + '\n*MATLAB*\n```MATLAB\n' + child.text + '\n```'
+
+        ct = textwrap.dedent(child.text)
+        if child.tag == 'matlab':
+            text = text + '\n*MATLAB*\n```MATLAB\n' + ct + '\n```'
         elif child.tag == 'schema':
-            text = text + '\n*Schema*\n```JSON\n' + child.text + '\n```'
+            text = text + '\n*Schema*\n```JSON\n' + ct + '\n```'
         elif child.tag == 'json':
-            text = text + '\n*JSON*\n```JSON\n' + child.text + '\n```'
+            text = text + '\n*JSON*\n```JSON\n' + ct + '\n```'
         elif child.tag == 'errors':
-            text = text + '\n*Errors*\n```MATLAB\n' + child.text + '\n```'
+            text = text + '\n*Errors*\n```MATLAB\n' + ct + '\n```'
 
     return mergeText( readme, sep, text )
 
-def processTests( readme, path ):
-    tree = ET.parse(path)
-    root = tree.getroot()
+def process( readme, sep ):
+    logging.info("Processing %s" % sep)
 
-    for child in root:
-        #if child.attrib.get('readme', 'false') == 'true':
-        readme = mergeElement(readme, child.find('description').text, child)
+    # sep is of the form
+    #   [//]: # "filename(#hash)?"
+    m = re.match('.*"(.*)"', sep)
+    sep = m.group(1)
+    parts = sep.split('#')
+
+    if len(parts) == 1:
+        f = codecs.open('test/' + sep, "r", "utf-8")
+        code = f.read()
+        f.close()
+        return mergeText( readme, sep, '\n```MATLAB\n' + code + '\n```' )
+    else:
+        testElem = xmlRoots[parts[0]].find( ".//*[description='%s']" % parts[1] )
+        readme = mergeElement(readme, sep, testElem)
 
     return readme
 
-def processFile( readme, sep ):
-    f = codecs.open(sep, "r", "utf-8")
-    code = f.read()
-    f.close()
-    return mergeText( readme, sep, '\n```MATLAB\n' + code + '\n```' )
+xmlRoots = dict()
+xmlRoots['testRoundtrip.xml'] = ET.parse('test/testRoundtrip.xml').getroot()
+xmlRoots['testParse.xml'] = ET.parse('test/testParse.xml').getroot()
+xmlRoots['testValidation.xml'] = ET.parse('test/testValidation.xml').getroot()
 
-readme = processTests(readme, 'test/testRoundtrip.xml')
-readme = processTests(readme, 'test/testParse.xml')
-readme = processTests(readme, 'test/testValidation.xml')
+seps = re.findall('\[//\]: # ".*"', readme)
+# print(seps)
 
-readme = processFile(readme, 'test/testErrorHandling.m')
-readme = processFile(readme, 'test/testUsage.m')
+for idx, sep in enumerate(seps):
+    if idx%2 == 0:
+        readme = process(readme, sep)
 
 f = codecs.open("README1.md", "w", "utf-8")
 f.write(readme)
