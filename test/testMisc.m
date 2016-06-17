069691c49f0aba3cdf3684c2f44dff25573c23a9
wolfgang kuehn <wolfgang@decatur.de>
2016-06-17 19:16:52 +0200

    Issue #6

--- test/testMisc.m
+++ test/testMisc.m
@@ -58,3 +58,9 @@ assert(isempty(errors));
 
 [obj, errors] =  JSON.parse(sprintf('"foo\tbar"'));
 assert(strcmp(errors{1}{2}, 'Invalid char found in range #00-#1F at line 1, column 5'));
+
+uriPrefix = ['file:///' strrep(pwd, '\', '/') '/test/'];
+
+[obj, errors] = JSON.parse([uriPrefix 'issue6/request.json'], [uriPrefix  'issue6/request_schema.json']);
+assert(isempty(errors));
+assert(isa(obj.start, 'datetime'));
