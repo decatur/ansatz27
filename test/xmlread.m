d860451a3954d4a8066a5096c700755631d04f2a
wolfgang <wolfgang@decatur.de>
2016-10-07 23:46:12 +0200

    Refactored tests

--- test/xmlread.m
+++
@@ -1,9 +0,0 @@
-function document = xmlread(filePath)
-
-factory = javaMethod('newInstance', 'javax.xml.parsers.DocumentBuilderFactory');
-builder = factory.newDocumentBuilder();
-
-file = javaObject('java.io.File', filePath);
-document = builder.parse(file);
-
-end
